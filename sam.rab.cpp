3596119f8ea0398e4a751d7d478caaac33bd2dde
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2021-11-01 09:24:04 +0300

    Add files via upload

---
+++ sam.rab.cpp
@@ -0,0 +1,14 @@
+﻿"num1.exe" (Win32). Загружено "C:\Users\User\source\repos\num1\Debug\num1.exe". Символы загружены.
+"num1.exe" (Win32). Загружено "C:\Windows\SysWOW64\ntdll.dll". 
+"num1.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel32.dll". 
+"num1.exe" (Win32). Загружено "C:\Windows\SysWOW64\KernelBase.dll". 
+"num1.exe" (Win32). Загружено "C:\Windows\SysWOW64\msvcp140d.dll". 
+"num1.exe" (Win32). Загружено "C:\Windows\SysWOW64\vcruntime140d.dll". 
+"num1.exe" (Win32). Загружено "C:\Windows\SysWOW64\ucrtbased.dll". 
+Поток 0x660 завершился с кодом 0 (0x0).
+"num1.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel.appcore.dll". 
+"num1.exe" (Win32). Загружено "C:\Windows\SysWOW64\msvcrt.dll". 
+"num1.exe" (Win32). Загружено "C:\Windows\SysWOW64\rpcrt4.dll". 
+Поток 0x1804 завершился с кодом 0 (0x0).
+Поток 0x2880 завершился с кодом 0 (0x0).
+Программа "[14076] num1.exe" завершилась с кодом 0 (0x0).
